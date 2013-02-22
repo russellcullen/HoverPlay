533d6ed4e1936ff55bcc800caef2e03d6f9992a5
Russell Cullen <russellscullen@gmail.com>
2013-02-22 14:49:20 -0500

    Options page changes

--- options/options.js
+++ options/options.js
@@ -1,27 +1,44 @@
-function save_options() {
+function show_save() {
+  $('#status').fadeIn().delay(750).fadeOut();
+}
+
+function update_delay() {
   localStorage.setItem('hover-delay', $('#delay').val());
-  // TODO: save disabled sites.
-  $('#status').fadeIn()
-  setTimeout(function() {
-    $('#status').fadeOut()
-  }, 750);
+  show_save();
+}
+
+function enable_site(div, key) {
+  div.remove();
+  if ($('.disabled-site').length == 0) {
+    $('#sites').append("None");
+  }
+  localStorage.setItem(key, 'false');
+  show_save();
 }
 
 function restore_options() {
   for (var key in localStorage) {
     if (key.search("disabled-") == 0 && localStorage[key] == 'true') {
       var div = $("<div class='disabled-site'>");
-      div.append(key.replace("disabled-", ""));
+      var button = $("<button class='right'>").text("Enable");
+      var url = key;
+      button.on('click', function() {
+        enable_site(div, url);
+      });
+      div.append(url.replace("disabled-", ""));
+      div.append(button);
       $('#sites').append(div);
     }
     if (key == "hover-delay"){
       $('#delay').val(localStorage[key]);
     }
   }
+  if ($('.disabled-site').length == 0) {
+    $('#sites').append("None");
+  }
 }
 
 $(function() {
   restore_options();
-  $('#save').click(save_options);
-  $('#delay').change(save_options);
+  $('#delay').on('change', update_delay);
 })
