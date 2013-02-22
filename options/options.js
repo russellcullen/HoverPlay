04fcace9318afd81af1c9676d0b57bd1bb830cf2
Russell Cullen <russellscullen@gmail.com>
2013-02-22 18:50:54 -0500

    Styling and bug fixes

--- options/options.js
+++ options/options.js
@@ -1,3 +1,21 @@
+function valid_url(url) {
+  return (url && url.replace(/\s+/, "") != "")
+}
+
+function add_site(key) {
+  if ($('.disabled-site').length == 0) {
+    $('#sites').empty();
+  }
+  var div = $("<div class='disabled-site'>");
+  var button = $("<button class='right'>").text("Enable");
+  button.on('click', function() {
+    enable_site(div, key);
+  });
+  div.text(key.replace("disabled-", ""));
+  div.append(button);
+  $('#sites').append(div);
+}
+
 function show_save() {
   $('#status').slideDown().delay(750).fadeOut();
 }
@@ -16,18 +34,20 @@ function enable_site(div, key) {
   show_save();
 }
 
+function disable_site() {
+  var url = $('#add-site-input').val();
+  if (valid_url(url) && localStorage.getItem("disabled-"+url) !=  'true') {
+    add_site("disabled-"+url);
+    localStorage.setItem("disabled-"+url, 'true');
+    show_save();
+  }
+  $('#add-site-input').val("");
+}
+
 function restore_options() {
   for (var key in localStorage) {
     if (key.search("disabled-") == 0 && localStorage[key] == 'true') {
-      var div = $("<div class='disabled-site'>");
-      var button = $("<button class='right'>").text("Enable");
-      var url = key;
-      button.on('click', function() {
-        enable_site(div, url);
-      });
-      div.append(url.replace("disabled-", ""));
-      div.append(button);
-      $('#sites').append(div);
+      add_site(key);
     }
     if (key == "hover-delay"){
       $('#delay').val(localStorage[key]);
@@ -41,4 +61,11 @@ function restore_options() {
 $(function() {
   restore_options();
   $('#delay').on('change', update_delay);
-})
+  $('#add-site-input').on('keypress', function (e) {
+    if (e.which == 13) {
+      e.preventDefault();
+      disable_site();
+    }
+  });
+  $('#add-site-btn').on('click', disable_site);
+});
