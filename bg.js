70f0362cb0687aeb64e3d2c57ecc9977e485fb2d
Russell Cullen <russellscullen@gmail.com>
2013-02-22 03:58:01 -0500

    Fix page action bug

--- bg.js
+++ bg.js
@@ -3,7 +3,7 @@ chrome.extension.onMessage.addListener(
     chrome.tabs.query({windowId: chrome.windows.WINDOW_ID_CURRENT, active: true}, function(tabs) {
       var tab = tabs[0]
       var domain = tab.url.match(/^[\w-]+:\/*\[?([\w\.:-]+)\]?(?::\d+)?/)[1];
-      if (request.show || localStorage.getItem("hoverzoom-"+domain) == "true") {
+      if (request.show || localStorage.getItem("hoverzoom-"+domain)) {
         chrome.pageAction.show(tab.id)
       }
       sendResponse({
