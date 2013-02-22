1fc24d0ef9ddaa63c6c59534bc8b9dac4fb4fc1e
Russell Cullen <russellscullen@gmail.com>
2013-02-22 00:10:18 -0500

    Bug fixes

--- bg.js
+++ bg.js
@@ -1,13 +1,13 @@
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if (request.info == "mode") {
-      chrome.tabs.getSelected(null, function(tab) {
-        var domain = tab.url.match(/^[\w-]+:\/*\[?([\w\.:-]+)\]?(?::\d+)?/)[1];
+chrome.tabs.getSelected(null, function(tab) {
+  var domain = tab.url.match(/^[\w-]+:\/*\[?([\w\.:-]+)\]?(?::\d+)?/)[1];
+  chrome.extension.onMessage.addListener(
+    function(request, sender, sendResponse) {
+      if (request.info == "mode") {
         sendResponse({
-          disabled: localStorage["audiomatic-"+domain],
-          isHoverMode: localStorage["isHoverMode"]
+          isHoverMode: localStorage["isHoverMode"],
+          disabled: localStorage["audiomatic-"+domain]
         });
-      });
+      }
     }
-  }
-);
+  );
+});
